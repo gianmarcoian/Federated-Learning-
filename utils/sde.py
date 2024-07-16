c906874cc661a8b8a74623325f8bfa0b0f5957d0
gianmarcoian <103632703+gianmarcoian@users.noreply.github.com>
2024-07-16 08:28:27 -0500

    Create sde.py

---
+++ utils/sde.py
@@ -0,0 +1,20 @@
+  def marginal_prob(self, x, t):
+    std = self.sigma_min * (self.sigma_max / self.sigma_min) ** t
+    mean = x
+    return mean, std
+
+  def prior_sampling(self, shape):
+    return torch.randn(*shape) * self.sigma_max
+
+  def prior_logp(self, z):
+    shape = z.shape
+    N = np.prod(shape[1:])
+    return -N / 2. * np.log(2 * np.pi * self.sigma_max ** 2) - torch.sum(z ** 2, dim=(1, 2, 3)) / (2 * self.sigma_max ** 2)
+
+  def discretize(self, x, t):
+    timestep = (t * (self.N - 1) / self.T).long()
+    sigma = self.discrete_sigmas.to(t.device)[timestep]
+    adjacent_sigma = torch.where(timestep == 0, torch.zeros_like(t),
+                                 self.discrete_sigmas[timestep - 1].to(t.device))
+    f = torch.zeros_like(x)
+    G = torch.sqrt(sigma ** 2 - adjacent_sigma ** 2)
