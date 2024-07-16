529d48b91fec0ff14d855163df9cacc4143aaa4a
gianmarcoian <103632703+gianmarcoian@users.noreply.github.com>
2024-07-16 08:27:52 -0500

    Update network.py

--- utils/network.py
+++ utils/network.py
@@ -1,20 +1,21 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import random
 import numpy as np
-import torch
+import functools
+from . import layers, normalization, recon_utils
 
 random.seed(0)
 np.random.seed(0)
 torch.manual_seed(0)
-
 
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.fc1 = nn.Linear(320,50)
+        self.fc1 = nn.Linear(320, 50)
         self.dropout = nn.Dropout(0.3)
         self.fc2 = nn.Linear(50, 10)
 
@@ -27,9 +28,6 @@ class CNN(nn.Module):
         x = self.fc2(x)
         return x
 
-
-class Unet():
-    pass
 
 RefineBlock = layers.RefineBlock
 ResidualBlock = layers.ResidualBlock
